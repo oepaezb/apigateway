9e92cbca744a8c89f0c469d9d792effea2171233
DESKTOP-CNPPB85\PC-OSCAR <oepaezb@gmail.com>
2024-12-10 12:54:19 -0500

    Correciones de compatibilidad

--- src/main/java/eis/gateway/ServletInitializer.java
+++ src/main/java/eis/gateway/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.gateway.gateway;
+package eis.gateway;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
