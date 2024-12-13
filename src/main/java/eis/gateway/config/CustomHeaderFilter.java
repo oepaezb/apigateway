080d5ad4600385784ffab15c38b70617e03d1d4b
alanfg <aherrera@asesoftware.com>
2024-12-13 08:46:35 -0500

    test

--- src/main/java/eis/gateway/config/CustomHeaderFilter.java
+++ src/main/java/eis/gateway/config/CustomHeaderFilter.java
@@ -1,2 +1,17 @@
-package eis.gateway.config;public class CustomHeaderFilter {
+package eis.gateway.config;
+
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class CustomHeaderFilter {
+
+    @Bean
+    public GlobalFilter removeDuplicateAccessControlAllowOriginHeader() {
+        return (exchange, chain) -> chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            exchange.getResponse().getHeaders().remove("Access-Control-Allow-Origin");
+        }));
+    }
 }
